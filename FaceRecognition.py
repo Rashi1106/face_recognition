587206b3c8a46253c39965dea0ece0f5555974db
Rashi1106 <60874438+Rashi1106@users.noreply.github.com>
2020-07-02 23:22:18 +0530

    Add files via upload

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -6,10 +6,20 @@ Created on Wed Jul  1 15:10:06 2020
 """
 import cv2
 import os
+import pickle
 
-Base_dir="C:\ProgramData\Anaconda3\Lib\site-packages\cv2\data\testimages.py"
+Base_dir="C:\\ProgramData\\Anaconda3\\Lib\\site-packages\\cv2\\data\\"
 image_dir= os.path.join(Base_dir,"testimages")
-faceCascade = cv2.CascadeClassifier("C:\ProgramData\Anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalcatface.xml")
+
+face_Cascade = cv2.CascadeClassifier("C:\ProgramData\Anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_alt.xml")
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+labels={"person_name": 1}
+with open("labels.pickle", 'rb')as f:
+    orig_labels = pickle.load(f)
+    labels = {v:k for k,v in orig_labels.items()}
 
 video_capture = cv2.VideoCapture(0)
 
@@ -19,7 +29,7 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
+    faces = face_Cascade.detectMultiScale(
         gray,
         scaleFactor=1.5,
         minNeighbors=5,
@@ -30,11 +40,24 @@ while True:
     for (x, y, w, h) in faces:
         roi_gray= gray[y:y+h,x:x+w]
         roi_color= frame[y:y+h,x:x+w]
-        img_item = "C:\ProgramData\Anaconda3\Lib\site-packages\cv2\data\testimages.py" + ".png"
+        #img_item = "C:\ProgramData\Anaconda3\Lib\site-packages\cv2\data\testimages.py" + ".png"
+        #cv2.imwrite(img_item,roi_gray)
+        id_, conf =recognizer.predict(roi_gray)
+        if conf>=45 and conf <= 85:
+            print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name=labels[id_]
+            color= (255,255,255)
+            stroke = 2
+            cv2.putText(frame,name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+        
+        img_item="my_image.png"
         cv2.imwrite(img_item,roi_gray)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
                                #endcord x endcord y color(BGR) strokes
     
+       
         
     # Display the resulting frame
     
