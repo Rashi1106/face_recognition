587cd6f43bf1a33739f034bd64f5c0b28d429066
Rashi1106 <60874438+Rashi1106@users.noreply.github.com>
2020-07-25 16:12:17 +0530

    Add files via upload

--- pi_face_recognition.py
+++ pi_face_recognition.py
@@ -18,7 +18,6 @@ ap.add_argument("-c", "--cascade", required=True,
 	help = "/home/rashi/pi-face-recognition/")
 ap.add_argument("-e", "--encodings", required=True,
 	help="/home/rashi/pi-face-recognition/")
-
 args = vars(ap.parse_args())
 
 # load the known faces and embeddings along with OpenCV's Haar
@@ -42,16 +41,15 @@ while True:
 	# to 500px (to speedup processing)
 	frame = vs.read()
 	frame = imutils.resize(frame, width=500)
-	
+
+
 	# convert the input frame from (1) BGR to grayscale (for face
 	# detection) and (2) from BGR to RGB (for face recognition)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
 	# detect faces in the grayscale frame
-	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
-	minNeighbors=5, minSize=(30, 30),
-		flags=cv2.CASCADE_SCALE_IMAGE)
+	rects = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),flags=cv2.CASCADE_SCALE_IMAGE)
 
 	# OpenCV returns bounding box coordinates in (x, y, w, h) order
 	# but we need them in (top, right, bottom, left) order, so we
@@ -61,13 +59,11 @@ while True:
 	# compute the facial embeddings for each face bounding box
 	encodings = face_recognition.face_encodings(rgb, boxes)
 	names = []
-	
-	
+	conf=[]	
 	# loop over the facial embeddings
 	for encoding in encodings:
 		matches = face_recognition.compare_faces(data["encodings"],encoding)
 		name = "Unknown"
-
 		# check to see if we have found a match
 		if True in matches:
 			# find the indexes of all matched faces then initialize a
@@ -77,31 +73,30 @@ while True:
 			counts = {}
 			
 			# loop over the matched indexes and maintain a count for
-			# each recognized face face
+			# each recognized face
 			for i in matchedIdxs:
 				name = data["names"][i]
+				confidence=["conf"]
 				confidence=((i/len(matches))*100) 
 				counts[name] = counts.get(name, 0) + 1
-				print(confidence)    
-			# determine the recognized face with the largest number
+				   # determine the recognized face with the largest number
 			# of votes (note: in the event of an unlikely tie Python
 			# will select first entry in the dictionary)
 			name = max(counts, key=counts.get)
-    	    
+   	    
 		# update the list of names
 		names.append(name)
-		
-        
-
+		conf.append(confidence)
+		   
 	# loop over the recognized faces
-	for ((top, right, bottom, left), name) in zip(boxes, names):
+	for ((top, right, bottom, left), name, confidence) in zip(boxes, names, conf):
 		# draw the predicted face name on the image
-		cv2.rectangle(frame, (left, top), (right, bottom),
-			(0, 255, 0), 2)
+		cv2.rectangle(frame, (left, top), (right, bottom),(0, 255, 0), 2)
 		y = top - 15 if top - 15 > 15 else top + 15
-		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-			0.75, (0, 255, 0), 2)
-
+		text = "{}: {:.2f}".format(name, confidence)
+		cv2.putText(frame, text,(left, y), cv2.FONT_HERSHEY_SIMPLEX,0.75, (0, 255, 0), 2)
+		#cv2.putText(frame, confidence,(left, y), cv2.FONT_HERSHEY_SIMPLEX,0.75, (255, 255, 0), 2)
+     
 	# display the image to our screen
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
@@ -118,6 +113,5 @@ fps.stop()
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-# do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
